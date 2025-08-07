5502e94505434cf71e47b28dddb1808875b82d08
jeff <aligwekwe2000j@gmail.com>
2025-08-07 12:05:32 +0100

    fix: added new backend url

--- lib/const.ts
+++ lib/const.ts
@@ -1,5 +1,5 @@
 export const BASE_URL =
-  process.env.BACKEND_URL || "https://land-use-api.onrender.com";
+  process.env.BACKEND_URL || "https://pre-release-land-biller-4n9mh.ondigitalocean.app";
 export const API_ROUTE = {
   auth: {
     login: "/api/auth/login",
