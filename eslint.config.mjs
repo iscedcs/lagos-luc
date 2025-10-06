27823579418f8d9c4062ed4c43db5c16bf6b224e
slim <aligwekwe2000@yahoo.com>
2025-10-06 15:46:43 +0100

    feat: update ESLint configuration to disable additional rules for React and TypeScript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,8 @@ const eslintConfig = [
       "react/no-unescaped-entities": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "off",
+    "@typescript-eslint/no-non-null-asserted-optional-chain": "off"
     },
   }),
 ];
